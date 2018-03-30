526b0e38d519168e995ba2dc303173b4317d994f
Angel Gabriel Abad Gomez <angel.abad@yandex.ru>
2018-03-30 18:58:27 +0300

    Listeners from the form

--- designs.js
+++ designs.js
@@ -8,6 +8,15 @@
 const columns = document.getElementById('inputWeight').value;
 const rows = document.getElementById('inputHeight').value;
 
+//set listeners from the form
+let form = document.querySelector('#sizePicker');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  //testing message
+  console.log("Funciona!");
+});
+
 // Select color input
 // Select size input
 
